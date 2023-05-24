c82124135017f5bdd41b9b7d31ffc8dfe0713eb2
leandrochs <leandrochagask@gmail.com>
2023-05-23 23:04:09 -0300

    feat: adiciona funcionalidades para modelagem

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,47 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+class Usuario(val nome: String) {
+    override fun toString(): String {
+        return nome
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("James")
+    val usuario2 = Usuario("Maria")
+    val conteudo1 = ConteudoEducacional("Java", 120, Nivel.INTERMEDIARIO)
+    val conteudo2 = ConteudoEducacional("Kotlin", 200, Nivel.BASICO)
+    val conteudos = listOf(conteudo1, conteudo2)
+
+    val formacao = Formacao("Formação Supimpa", conteudos)
+
+    var conteudosFormatados = formacao.conteudos.joinToString("\n") { "${it.nome} (${it.duracao} minutos) ${it.nivel}" }
+    println("Conteúdos da formação:\n$conteudosFormatados\n")
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    println("Usuário(s) matriculado(s) na formação:\n${formacao.inscritos.joinToString("\n")}\n")
+
+    val conteudo3 = ConteudoEducacional("Docker", 90, Nivel.INTERMEDIARIO)
+    val conteudo4 = ConteudoEducacional("Método Ágil", 150, Nivel.BASICO)
+    val novosConteudos = listOf(conteudo3, conteudo4)
+
+    formacao.conteudos = novosConteudos
+
+    conteudosFormatados = formacao.conteudos.joinToString("\n") { "${it.nome} (${it.duracao} minutos) ${it.nivel}" }
+    println("Conteúdos atualizados da formação:\n$conteudosFormatados\n")
 }
